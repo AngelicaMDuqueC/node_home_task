1fcd271e47ef5fc0dfcc0de9818667249483ab77
angelica_duque <angelica_duque@epam.com>
2022-12-12 14:05:35 -0500

    created a readFile for cvs

---
+++ src/readFile.js
@@ -0,0 +1,27 @@
+const csv = require('csvtojson')
+const fs = require('fs')
+
+const csvFilePath = '/cvs/nodejs-hw1-ex1.csv';
+const txtFilePath = '/cvs/nodejs-hw1-ex1.txt';
+const readStreamCvs = fs.createReadStream(`${__dirname}${csvFilePath}`);
+const writeStream = fs.createWriteStream(`${__dirname}${txtFilePath}`)
+
+csv()
+  .fromStream(readStreamCvs)
+  .subscribe(json => {
+    const txtfile = writeStream;
+    txtfile.write(JSON.stringify(json, null, 4));
+  })
+  .on('error', (error) => {
+    throw Error(`Error reading CSV file: ${error.message}`)
+  })
+
+readStreamCvs
+  .on('error', (error) => {
+      throw Error(`Error reading CSV file: ${error.message}`)
+  })
+
+writeStream
+  .on('error', (error) => {
+      throw Error(`Error reading CSV file: ${error.message}`)
+  })
