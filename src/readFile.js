43c3db3cc3d813773db33dacbed8714c8a27345b
angelica_duque <angelica_duque@epam.com>
2022-12-12 14:48:17 -0500

    recoding using es6 and pipeline with stream

--- src/readFile.js
+++ src/readFile.js
@@ -1,20 +1,20 @@
-const csv = require('csvtojson')
-const fs = require('fs')
+import * as fs from 'fs';
+import csv from 'csvtojson';
 
 const csvFilePath = '/cvs/nodejs-hw1-ex1.csv';
 const txtFilePath = '/cvs/nodejs-hw1-ex1.txt';
 const readStreamCvs = fs.createReadStream(`${__dirname}${csvFilePath}`);
 const writeStream = fs.createWriteStream(`${__dirname}${txtFilePath}`)
 
-csv()
-  .fromStream(readStreamCvs)
-  .subscribe(json => {
-    const txtfile = writeStream;
-    txtfile.write(JSON.stringify(json, null, 4));
-  })
-  .on('error', (error) => {
-    throw new Error(`Error reading CSV file: ${error.message}`)
-  })
+readStreamCvs
+.pipe(csv())
+.pipe(writeStream)
+.on('error', (error) => {
+  throw new Error(`Error creating CSV file: ${error.message}`)
+})
+.on('end', () => {
+console.log('csv file successfully converted to json');
+});
 
 readStreamCvs
   .on('error', (error) => {
